1827eef39f51fd1fa21341386ac41b2b8cc241ae
Alfredo-Rojas <alfredot84@gmail.com>
2020-01-30 11:27:19 -0500

    Staring with C

---
+++ C/examples.c
@@ -0,0 +1,40 @@
+// #include <stdio.h>
+// #include <cs50.h>
+
+// int main(void)
+// {
+//   printf("hello, world\n");
+// }
+
+// if (x < Y)
+// {
+//   printf("x is less than y\n");
+// }
+// else if (x > y)
+// {
+//   printf("x is greater than y\n");
+// }
+// else
+// {
+//   printf("x is equal to y\n");
+// }
+
+// while (true) {
+//   printf("Hello, world\n");
+// }
+
+// for (int i=0;i<=50;i++) {
+//   printf("hello, world\n");
+// }
+
+// int main(void) {
+
+//     string name = get_string("What is your name?\n");
+//     printf("hello, %s\n", name);
+// }
+
+// int main(void) {
+
+//     string name = get_string("What is your name?\n");
+//     printf("hello, %s\n", name);
+// }
