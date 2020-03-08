e9fd28b59fccd11c2d33447bd83b871355ac6fb5
Alfredo-Rojas <alfredot84@gmail.com>
2020-03-07 22:10:10 -0500

    Two small programs added

--- C/examples.c
+++ C/examples.c
@@ -1,6 +1,9 @@
 // #include <stdio.h>
 // #include <cs50.h>
 
+//////to compile de program enter: make <filename>
+//////to run the program: ./filename
+
 // int main(void)
 // {
 //   printf("hello, world\n");
@@ -19,12 +22,16 @@
 //   printf("x is equal to y\n");
 // }
 
-// while (true) {
-//   printf("Hello, world\n");
+//////While looop
+// int i = 0;
+// while (i < 50) {
+//     printf("%i hello, world\n", i);
+//     i++;
 // }
 
-// for (int i=0;i<=50;i++) {
-//   printf("hello, world\n");
+//////For looop
+// for (int i = 0;i <= 50;i++) {
+//   printf("%i hello, world\n", i);
 // }
 
 // int main(void) {
@@ -37,4 +44,22 @@
 
 //     string name = get_string("What is your name?\n");
 //     printf("hello, %s\n", name);
+// }
+
+///////Small program "How many days old"
+
+// int main(void) {
+
+//   int age = get_int("What's your age?\n");
+//   int daysOld = age * 365;
+//   printf("You are at least %i days old.\n", daysOld);
+// }
+
+////////Small program "What's the price"
+
+// int main(void)
+// {
+
+//   float price = get_float("What's the price?\n");
+//   printf("Your total is %.2f.\n", price * 1.07);
 // }
