ebca5b95f00eac413d7eb85b03dc1ca112a0abe6
Alfredo-Rojas <alfredot84@gmail.com>
2020-03-18 07:59:41 -0400

    String to uppercase added

--- C/examples.c
+++ C/examples.c
@@ -123,3 +123,29 @@
 // while (n < 1);
 //     return n;
 // }
+
+/////////Changing a String input from lowercase to uppercase
+// #include <cs50.h>
+// #include <stdio.h>
+// #include <string.h>
+// #include <ctype.h>
+
+// int main(void)
+// {
+//     string s = get_string("Input: ");
+//     printf("Output: ");
+//     for (int i = 0, n = strlen(s); i < n; i++)
+//     {
+//         printf("%c", toupper(s[i]));
+
+//         //Low level solution
+//         // if (s[i] >= 'a' && s[i] <= 'z')
+//         // {
+//         //     printf("%c", s[i] - 32);
+//         // } else
+//         // {
+//         //     printf("%c", s[i]);
+//         // }
+//     }
+//     printf("\n");
+// }
