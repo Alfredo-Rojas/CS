012535d436500ebb04e5cec57f3c3b9f0d172d36
Alfredo-Rojas <alfredot84@gmail.com>
2020-03-23 00:39:31 -0400

    String comparison added

--- C/examples.c
+++ C/examples.c
@@ -165,3 +165,25 @@
 //         printf("hello, world\n");
 //     }
 // }
+
+///////////String Comparison/////
+
+// #include <cs50.h>
+// #include <stdio.h>
+// #include <string.h>
+
+// int main(void)
+// {
+//   string names[4] = {"EMMA", "RODRIGO", "BRIAN", "DAVID"};
+
+//   for (int i = 0; i < 4; i++)
+//   {
+//     if (strcmp(names[i], "EMMA") == 0)
+//     {
+//       printf("Found\n");
+//       return 0;
+//     }
+//   }
+//   printf("Not found\n");
+//   return 1;
+// }
